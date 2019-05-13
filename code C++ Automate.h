e590a79650406ec572730ac6a4d56fb7c99e22de
cocotin <50593135+cocotin@users.noreply.github.com>
2019-05-13 19:59:29 +0200

    Create code C++ Automate.h

---
+++ code C++ Automate.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Automate
+{
+public:
+    Automate(string fichier_source);
+    int est_un_automate_asynchrone();
+    int est_un_automate_deterministe();
+    int est_un_automate_complet();
+    ~Automate();
+    void afficher();
+private:
+    string nom_fichier_source;
+    int taille_alphabet;
+    int nb_etats;
+    int nb_etats_initiaux;
+    int* etats_initiaux;
+    int nb_etats_terminaux;
+    int* etats_terminaux;
+    int nb_transitions;
+    int** transitions;
+};
