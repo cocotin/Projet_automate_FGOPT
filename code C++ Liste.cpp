64797618fdb41e49f33c4fc6d0bc85031663e544
cocotin <50593135+cocotin@users.noreply.github.com>
2019-05-17 12:03:58 +0200

    Create code C++ Liste.cpp

---
+++ code C++ Liste.cpp
@@ -0,0 +1,24 @@
+#include "Liste.h"
+
+Liste::Liste(int* petat, int nb_etats, int ptaille_alphabet)
+{
+    taille_alphabet = ptaille_alphabet;
+    taille_etat = nb_etats;
+    etat = (int*)malloc(taille_etat * sizeof(int));
+    for (int i = 0; i < taille_etat; i++) {
+        etat[i] = petat[i];
+    }
+    transitions = (int**)malloc(taille_alphabet * sizeof(int*));
+    nb_etats_superposes = (int*)malloc(taille_alphabet * sizeof(int));
+    suivant = NULL;
+}
+
+Liste::~Liste()
+{
+    for (int i = 0; i < taille_alphabet; i++) {
+        free(transitions[i]);
+    }
+    free(etat);
+    free(nb_etats_superposes);
+    free(transitions);
+}
