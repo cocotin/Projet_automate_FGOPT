09a6f1c056f7cf58743b36aeed21cefb24883612
cocotin <50593135+cocotin@users.noreply.github.com>
2019-05-17 12:05:00 +0200

    Update code C++ main.cpp

--- code C++ main.cpp
+++ code C++ main.cpp
@@ -7,21 +7,6 @@
 
 using namespace std;
 
-void aff_fichier(string fichier_source) {
-    ifstream fichier(fichier_source.c_str(), ios::in);
-    if(fichier)
-    {
-        string ligne;
-        while(getline(fichier, ligne))
-        {
-            cout << ligne << endl;
-        }
-        fichier.close();
-    } else {
-        cerr << "Impossible d'ouvrir le fichier \"" << fichier_source << "\"" << endl;
-    }
-}
-
 void menu() {
     printf("Quel automate voulez-vous utiliser?\nindiquez son numero(0 pour quitter) : ");
     char reponse[20] = "";
@@ -33,25 +18,24 @@ void menu() {
     }
     string nom_fichier = "L2-F1-" + (string)reponse + ".txt";
     cout << nom_fichier << endl;
-    aff_fichier(nom_fichier);
     Automate af(nom_fichier);
     if (!af.correct) menu();
     af.afficher();
-    Automate_dc afdc;
+    Automate_dc* afdc;
     if (af.est_un_automate_asynchrone()) {
-        afdc = Automate_dc(af, 'a');
+        afdc = new Automate_dc(af, 'a');
     } else {
         if (af.est_un_automate_deterministe()) {
             if (af.est_un_automate_complet()) {
-                afdc = Automate_dc(af, 'n');
+                afdc = new Automate_dc(af, 'n');
             } else {
-                afdc = Automate_dc(af, 'c');
+                afdc = new Automate_dc(af, 'c');
             }
         } else {
-            afdc = Automate_dc(af, 's');
+            afdc = new Automate_dc(af, 's');
         }
     }
-    afdc.afficher();
+    afdc->afficher();
     menu();
 }
 
