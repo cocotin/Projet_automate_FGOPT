b8cafa8587370780b4cc7adc6ce010ecf2dc4a09
cocotin <50593135+cocotin@users.noreply.github.com>
2019-05-17 12:04:19 +0200

    Create code C++ Liste.h

---
+++ code C++ Liste.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Liste
+{
+public:
+    Liste(int* petat, int nb_etats, int ptaille_alphabet);
+    ~Liste();
+    int taille_alphabet;
+    int taille_etat;
+    int* etat;
+    int* nb_etats_superposes;
+    int** transitions;
+    Liste* suivant;
+};
